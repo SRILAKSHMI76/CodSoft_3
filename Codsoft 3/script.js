8d0ce6e341ffc15bca2517d89e3118af6998cc53
SRILAKSHMI76 <146519710+SRILAKSHMI76@users.noreply.github.com>
2024-09-05 18:33:56 +0530

    Add files via upload

---
+++ Codsoft 3/script.js
@@ -0,0 +1,11 @@
+// script.js
+
+// Example: Adding some interactivity like smooth scroll
+document.querySelectorAll('nav a').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
